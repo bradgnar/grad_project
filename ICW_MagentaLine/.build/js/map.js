b0e0f8f08915c28ca8f5ff76f02fad3b5e5bfef9
bradleysmith <bradleysmith@paypal.com>
2014-12-28 11:22:49 -0500

    making a commit before computer conks out.  Have changed a lot of the css, and a lot of the html.  Will need to change the folder structure of the app, and also place the code in different files than they are now... it looks like map.dust has basically become my master and that this will be a single page app.

--- ICW_MagentaLine/.build/js/map.js
+++ ICW_MagentaLine/.build/js/map.js
@@ -1,9 +1,25 @@
 function initialize() {
     var mapOptions = {
       center: { lat: 33.9, lng: -78.38},
-      zoom: 10
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 }
+
+//you can do a media query with the CSS to get this effect
+//no need for the JS
+// function detectBrowser() {
+//     var useragent = navigator.userAgent;
+//     var mapdiv = document.getElementById("map-canvas");
+
+//     if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+//         mapdiv.style.width = '100%';
+//         mapdiv.style.height = '100%';
+//     } else {
+//         mapdiv.style.width = '600px';
+//         mapdiv.style.height = '800px';
+//     }
+// }
 google.maps.event.addDomListener(window, 'load', initialize);
