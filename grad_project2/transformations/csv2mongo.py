7107dff1927d181284d66b9f048f16321d4362c1
bradleysmith <bradleysmith@paypal.com>
2015-01-10 15:06:15 -0500

    moving data files updating scripts and readme

--- grad_project2/transformations/csv2mongo.py
+++ grad_project2/transformations/csv2mongo.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import os
 import csv
-from pymongo import MongoClient, GEOSPHERE 
+from pymongo import MongoClient, GEOSPHERE
 from datetime import datetime
 
 client = MongoClient('localhost', 27017)
-db = client.project_db
+db = client.icw
 db.proto_sounds.drop()
 db.proto_sounds.create_index([("loc", GEOSPHERE)])
 
@@ -17,11 +21,23 @@ def createSoundingObj(row):
 		"Time": row[4]
 		}
 
-with open('/home/bradgnar/grad_project2/data/DB5_Brad_Towson.csv', 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    for row in csvreader:
-		obj = createSoundingObj(row)
-		db.proto_sounds.insert(obj)
-		# These are the headers for the csv file that matters
-		# if u lose headers just re download the csv
-    		# Latitude,Longitude,WLDepth_ft,Date,Time,StaticDraft,Vessel
+
+
+def main(argv):
+	print 'argv1 is' + argv[1]
+	print 'path is ' + os.path.expanduser('~'+argv[1])
+	print 'file dir ' + os.path.dirname(os.path.realpath(__file__))
+	print 'cwd dir ' + os.getcwd()
+	os.chdir(os.path.expanduser('~'+argv[1]))
+	print 'cwd dir ' + os.getcwd()
+
+	# '/Users/bradleysmith/workspace/grad_project/data/DB_5_Brad_Towson.csv'
+	# os.path.expanduser('~' + argv[1])
+	# with open('/Users/bradleysmith/workspace/grad_project/data/DB_5_Brad_Towson.csv', 'rb') as csvfile:
+	#     csvreader = csv.reader(csvfile, delimiter=',')
+	#     for row in csvreader:
+	# 		obj = createSoundingObj(row)
+	# 		db.proto_sounds.insert(obj)
+
+if __name__ == "__main__":
+	main(sys.argv)
--- scripts/README.md
+++ scripts/README.md
@@ -1,4 +1,4 @@
-#SCRIMPS
+#Scripts
 -------------------------------------------------------------------------------
 
 This will be a step by step tutorial/guide for setting up the database for the
@@ -9,5 +9,22 @@ version and as the scripts get better the tutorial [should] get better.
 [Mac OSX setup](http://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/ "mongoDB on Mac")
 [Ubuntu setup](http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/ "mongoDB on Ubuntu")
 [Windows setup](http://docs.mongodb.org/manual/tutorial/install-mongodb-on-windows/ "mongoDB on Windows")
+
+**Next** create a database - you can name it anything, but I made it with
+`use icw_magenta` while running in the mongodb shell
+
+###Script Time
+-------------------------------------------------------------------------------
+Now comes the fun time - running custom scripts
+
+####Get the data in there
+You are going to need to email survice engineering for the data files.  Once you have them you will need to install [pymongo](http://api.mongodb.org/python/2.0.1/installation.html)
+
+####Buoys
+run 'python csv2mongo.py /the/path/to/your/file'
+
+####Putting in your csv
+
+`python csv2mongo.py filepath/of/csv`
 
 
